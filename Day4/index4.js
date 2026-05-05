11e8f5f8b6c4211d4f82ccd6f326036db1c6d92f
Mahalaxmi <mahalaxmisagarla@gmail.com>
2026-05-05 12:42:28 +0530

    Auto-commit

--- Day4/index4.js
+++ Day4/index4.js
@@ -1,21 +1,28 @@
-function getData(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Data received")
-        }, 2000) // 2000 milliseconds = 2 seconds
-    });
-}
+// function getData(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Data received")
+//         }, 2000) // 2000 milliseconds = 2 seconds
+//     });
+// }
 
-getData().then((data) => {
-    console.log(data);      // Output: "Data received" after 2 seconds
-}).catch((error) => {
-    console.error("Error:", error); //If reject is called, this will run
-});
+// getData().then((data) => {
+//     console.log(data);      // Output: "Data received" after 2 seconds
+// }).catch((error) => {
+//     console.error("Error:", error); //If reject is called, this will run
+// });
 
-// setTimeout(() => {
-//     console.log("Timeout completed"); //runs after 3 seconds
-// }, 3000); 
+// // setTimeout(() => {
+// //     console.log("Timeout completed"); //runs after 3 seconds
+// // }, 3000); 
 
-// setInterval(() => {
-//     console.log("Interval running"); //runs every 3 seconds
-// }, 3000);
+// // setInterval(() => {
+// //     console.log("Interval running"); //runs every 3 seconds
+// // }, 3000);
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(res => res.json())
+.then(data => data.filter(d => d.id <= 5))
+.then(data => data.map(d => d.title))
+.then(data => console.log(data))
+.catch(error => console.error("Error fetching data:", error));
