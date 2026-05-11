937cf9107929d0ad77e56d02480d2e338ed9c44f
Mahalaxmi <mahalaxmisagarla@gmail.com>
2026-05-11 12:51:21 +0530

    Intro to ts

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,12 @@
 let name = "Maha"
-console.log(name)
+console.log(name);
+
+let result: number | string;
+result = 45;
+result = "success"
+console.log(name, result);
+
+function add(a:number, b:number): string{
+    return "Sum:" +(a+b);
+}
+console.log(add(5,10));
